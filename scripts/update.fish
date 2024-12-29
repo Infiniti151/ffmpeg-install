efce788e2e5b733a2fec828d7910be4acff2b5f3
Aadi <aadishchaubal@hotmail.com>
2024-12-29 12:52:33 -0500

    fix update script to update to the latest release branch

--- scripts/update.fish
+++ scripts/update.fish
@@ -1,15 +1,26 @@
 #!/usr/bin/fish
 
+function update -d "update ffmpeg"
+	sudo make uninstall
+	rm rf ./build/\*
+	./configure --enable-nonfree --enable-ffnvcodec --enable-libx264 --enable-libx265 --enable-cuda-llvm --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
+	make -j 8
+	sudo make install
+	echo -e '\e[32mSuccessfully updated FFMpeg\e[0m' 
+	sudo ldconfig
+	ffmpeg
+end
+
 cd ~/ffmpeg
-if [ (git fetch --dry-run) = '' ]
-    echo -e '\e[31mNo update found\e[0m'
+set -l latest (git pull > /dev/null && git branch -a | grep 'release' | tail -1 | string sub -s 18)
+if test "$(git branch --show-current)" = $latest 
+	if test -z (git fetch --dry-run)
+	    echo -e '\e[31mNo update found\e[0m'
+	else
+	    git pull
+	    update
+	end
 else
-    sudo make uninstall
-    rm rf ./build/*
-    git pull
-    ./configure --enable-nonfree --enable-ffnvcodec --enable-cuda-llvm --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
-    make -j 8
-    sudo make install
-    echo -e '\e[32mSuccessfully updated FFMpeg\e[0m' 
-    sudo ldconfig
-    ffmpeg
+	git switch $latest
+	update
+end
